9457007307518c7f94e1b124677c729b3a97654b
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-06-18 00:29:23 +0500

    navbar bitdi

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css"
+
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <>
+     <header>
+      <Navbar />
+     </header>
+    </>
+  )
+}
+
+export default App
