2a1b4368676e1de0625c677ab71636416a554e7f
Akmalov-dev <amirbekdebruyne@gmail.com>
2026-06-21 18:29:43 +0500

    products

--- src/Components/Middle/middle.jsx
+++ src/Components/Middle/middle.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import '../Middle/middle.css'
 import logo from "../Middle/image/logo.png"
 
+import p1 from '../Middle/image/product1.jpg'
+import p2 from '../Middle/image/product2.jpg'
+import p3 from '../Middle/image/product3.jpg'
+import p4 from '../Middle/image/product4.jpg'
+import p5 from '../Middle/image/product5.jpg'
+import p6 from '../Middle/image/product6.jpg'
+
+
 const middle = () => {
   return (
     <>
@@ -14,6 +22,15 @@ const middle = () => {
                 <button className='btn-instagram'><a className='link-instagram' href="https://www.instagram.com/aqvo_jiz/">Bizning Instagram</a></button>
                 <button className='btn-telegram'><a className='link-telegram' href="">Bizning Telegram</a></button>
             </div>
+        </div>
+
+        <div className='products'>
+            <img className='p1' src={p1} alt="alt" />
+            <img className='p1' src={p2} alt="alt" />
+            <img className='p1' src={p3} alt="alt" />
+            <img className='p1' src={p4} alt="alt" />
+            <img className='p1' src={p5} alt="alt" />
+            <img className='p1' src={p6} alt="alt" />
         </div>
     </>
   )
